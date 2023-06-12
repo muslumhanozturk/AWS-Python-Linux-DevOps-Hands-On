cde15683594f98da299883dc20fa8aa0c473f6a9
Muslum <muslumhanozturk@gmail.com>
2023-06-12 23:42:24 +0300

    12haz

--- Linux+/projects/Project-101/Second_Part/user_passwd.sh
+++ Linux+/projects/Project-101/Second_Part/user_passwd.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 #
 # This script creates a new user on the local system.
-# You will be prompted to enter the username (login), the person name, and a password.
+# You will be prompted to enter the username (login), the person name, and a password.      
 # The username, password, and host for the account will be displayed.
 
 # Make sure the script is being executed with superuser privileges.
-
+if [[ $UID -ne 0 ]]
+then
+        echo "Please run this script with superuser privileges."
+        exit 1
+fi
 # Get the username (login).
-
+read -p "Enter the username to create: " username
 # Get the real name (contents for the description field).
-
+read -p "Enter person name: " real_name
 # Get the password.
-
+read -sp "Enter your password: " password
 # Create the account.
-
+useradd -c $real_name -m $username 2> /dev/null
 # Check to see if the useradd command succeeded.
-# We don't want to tell the user that an account was created when it hasn't been.
-
+# We dont want to tell the user that an account was created when it hasn't been.
+if [[ $? -ne 0 ]]
+then
+        echo -e "\nThis username is already exists."
+        exit 1
+fi
 # Set the password.
-
-# Check to see if the passwd command succeeded.
-
-# Force password change on first login.
-
-# Display the username, password, and the host where the user was created.
-
-# If you get bad interpreter error: "sed -i -e 's/\r$//' <script_name>" command first.
--- Linux+/projects/Project-101/Third_Part/backup.sh
+++ Linux+/projects/Project-101/Third_Part/backup.sh
@@ -1,23 +1,51 @@
 #!/bin/bash
 
 # Check if we are root privilage or not
-
+if [[ $UID -ne 0 ]]
+then
+        echo "Please run this script with superuser privileges."
+        exit 1
+fi
 # Define the backup directory
-
+BACKUP_DIR="/mnt/backup"
 # Define the directories to be backed up
-
+DIRECTORIES=("/home/ec2-user/data")
 # Get the hostname of the instance
-
+HOSTNAME=$(hostname -s)
 # Get the current date and time
-
+DATE=$(date +%F-%H-%M)
 # Print start status message.
-
+echo "We will back up ${DIRECTORIES[@]} to under ${BACKUP_DIR}"
 # Create the backup directory if it doesn't exist
-
+if [ ! -d "$BACKUP_DIR" ]; then
+    mkdir "$BACKUP_DIR"
+fi
 # Iterate through the directories to be backed up
-
+for DIR in "${DIRECTORIES[@]}"
+do
+    # Create the filename for the backup
+    FILENAME="$BACKUP_DIR/$HOSTNAME-$DATE-$(basename $DIR).tgz"
+    tar -zcvf "$FILENAME" "$DIR"
+done
 # Or if you have only one directory that you know.
 
 # Run "sed -i -e 's/\r$//' scriptname.sh" command before running script.
 
 # Print end status message.
+echo "Back up is ready."
+
+
+
+  106  touch /home/ec2-user/data/file{1..10}
+  107  ls /home/ec2-user/data
+  112  tar -tzvf /mnt/bacpup/ip-172-31-87-66-2023-06-12-19-43-data.tgz
+  113  tar -tzvf /mnt/backup/ip-172-31-87-66-2023-06-12-19-43-data.tgz
+  
+sudo yum install cronie -y
+sudo systemctl enable crond.service
+sudo systemctl start crond.service
+sudo systemctl status crond.service
+crontab -e
+   */1 * * * * sudo /home/ec2-user/Project/Third_Part/backup.sh
+crontab -l
+ls /mnt/backup
